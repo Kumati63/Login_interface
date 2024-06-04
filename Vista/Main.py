8d66fe78eb208e9013a568be890a14b83f7cec55
Kumati <matiasarayao625@gmail.com>
2024-06-04 00:29:07 -0400

    mejoras al login
    agregado bordes a los entry y agrandado estos mismos

--- Vista/Main.py
+++ Vista/Main.py
@@ -12,10 +12,10 @@ def Login():# Esta función se ejecutará cuando se presione el botón
     login_window.geometry("400x450")
     login_window.config(bg="white")
 
-    Frame_up = Frame(login_window, width=400, height=50, bg="white")
+    Frame_up = Frame(login_window, width=400, height=70, bg="white")
     Frame_up.grid(row=0, column=0)
 
-    Frame_down = Frame(login_window, width=400, height=400, bg="white")
+    Frame_down = Frame(login_window, width=400, height=380, bg="white")
     Frame_down.grid(row=1, column=0)
 
     # declaring string variable
@@ -41,42 +41,54 @@ def Login():# Esta función se ejecutará cuando se presione el botón
             print("Please enter")
 
 
-    Header = Label(Frame_up, text="LOGIN", font=('Poppins', 23, "bold"), bg="white").place(x=40, y=5)
+    Header = Label(Frame_up, text="LOGIN", font=('Poppins', 23, "bold"), bg="white").place(x=40, y=20)
 
-    line = Label(Frame_up, width=40, text="", height=1, bg="#6757FF", anchor=NW)
-    line.place(x=35, y=45)
+    line = Label(Frame_up, width=40, text="", height=1, bg="#3fb5a3", anchor=NW)
+    line.place(x=35, y=65)
 
-    Email_label = Label(Frame_down, text='Email',font=('calibre', 10, 'bold'), bg="white").place(x=80, y=45)
+    Email_label = Label(Frame_down, text='Email',font=('calibre', 11, 'bold'), bg="white").place(x=70, y=45)
 
-    name_entry = Entry(Frame_down, textvariable=Email_var,
-                       font=('calibre', 10, 'normal'),
+    name_entry = Entry(Frame_down,
+                       textvariable=Email_var,
+                       font=('calibre', 17, 'normal'),
                        bg="white",
-                       width=35).place(x=75, y=70)
+                       borderwidth=2,
+                       width=20).place(x=55, y=70)
     #if name_entry=='':
 
-
     #   creating a label for password
-    passw_label = Label(Frame_down, text='Password', font=('calibre', 10, 'bold'), bg="white").place(x=75, y=95)
+    passw_label = Label(Frame_down, text='Password', font=('calibre', 11, 'bold'), bg="white").place(x=65, y=118)
     pass_F = Button(Frame_down,
                     text='Forgot Password',
                     command=F_password(),
                     font=('calibre', 9, 'bold'),
+                    activebackground="White",
+                    activeforeground="#4641FF",
                     bg="white",
-                    fg="blue").place(x=200, y=100)
+                    fg="#0800FF",
+                    highlightthickness = 0,
+                    bd = 0).place(x=200, y=120)
 
     # creating an entry for password
     passw_entry = Entry(Frame_down,
                         textvariable=passw_var,
-                        font=('calibre', 10, 'normal'),
+                        font=('calibre', 17, 'normal'),
                         show='*',
+                        borderwidth=2,
                         bg="white",
-                        width=35).place(x=75, y=125)
+                        width=20).place(x=55, y=145)
 
     # creating a button using the widget
     # Button that will call the submit function
     sub_btn = Button(Frame_down,
                      text='Submit',
-                     command=submit()).place(x=105, y=160)
+                     font=('calibre', 10, 'bold'),
+                     width=15,
+                     bg="#3fb5a3",
+                     fg="white",
+                     activebackground="#47DFC8",
+                     activeforeground="white",
+                     command=submit()).place(x=125, y=195)
 
     # placing the label and entry in
     # the required position using grid
